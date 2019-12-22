48f0190709e304b1346393615386d3bf67b6cc8c
jack <jack@poly.localdomain>
2019-12-22 17:10:04 -0600

    fixing comments, added requirements

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ttrss.client import TTRClient
+from ttrss.client import TTRssClient
 import configparser
 from requests.exceptions import ConnectionError
 import requests
@@ -17,185 +17,212 @@ password = config['Main']['Password']
 client = TTRClient(url, username, password)
 client.login()
 
-def get_feeds_from_config(config):                                      # makes a dict of feed names, count to keep, and filter patterns, release type,
-        l = []
-        for key in config['Feeds']:
-                name = config['Feeds'][key]
-                count = config[name]['count']
-                get_filter = config[name]['filter']
-                release_type = config[name]['release_type']
-                l.append({'feed_name': name, 'count':count, 'filter': get_filter, 'release_type':release_type}) #feed_name,count,filter_release_type
-        return l
 
-def mark_article_read(client, id):                                      # only function so far that actually needs to support str AND list
-        if type(id) == list:
-                for article in id:
-                        if(article.unread == True):
-                                article.toggle_unread()
-        else:     
-                
-                article = client.get_articles([id])[0]
-                if(article.unread == True):
-                        article.toggle_unread()
-                
+# makes a dict of feed names, count to keep, and filter patterns, release type,
+def get_feeds_from_config(config):
+    l = []
+    for key in config['Feeds']:
+        name = config['Feeds'][key]
+        count = config[name]['count']
+        get_filter = config[name]['filter']
+        release_type = config[name]['release_type']
+        l.append({'feed_name': name, 'count': count, 'filter': get_filter,
+                  'release_type': release_type})  # feed_name,count,filter_release_type
+    return l
+
+
+# only function so far that actually needs to support str AND list
+def mark_article_read(client, id):
+    if type(id) == list:
+        for article in id:
+            if(article.unread == True):
+                article.toggle_unread()
+    else:
+        article = client.get_articles([id])[0]
+        if(article.unread == True):
+            article.toggle_unread()
+
+
 def str_to_date(string):
-        datetime_object = datetime.strptime(string,'%Y-%m-%d %X')
-        return datetime_object
+    datetime_object = datetime.strptime(string, '%Y-%m-%d %X')
+    return datetime_object
 
-def download_item(url,feed_name):
-        print(url)
-        response = requests.get(url, stream=True, allow_redirects=True) # grabs item to be downloaded as a stream
-        local_filename = feed_name.replace(' ','_')+url.split('/')[-1]  # grabs the last element of an url and replaces chars
-        if os.path.isfile(local_filename):                              # will not overwrite files, even if they are zero btyes...
-                return local_filename
-        handle = open(local_filename, "wb")
-        for chunk in response.iter_content(chunk_size=1024*1024):       # download the item and write to a file
-                if chunk:  # filter out keep-alive new chunks
-                        handle.write(chunk)
-        handle.close()
+
+def download_item(url, feed_name):
+    # grabs the last element of an url and replaces chars
+    local_filename = feed_name.replace(' ', '_')+url.split('/')[-1]
+    # will not overwrite files, even if they are zero btyes...
+    if os.path.isfile(local_filename):
         return local_filename
+    # grabs item to be downloaded as a stream
+    with requests.get(url, stream=True, allow_redirects=True) as response:
+
+        with open(local_filename, "wb") as handle:
+            # download the item and write to a file
+            for chunk in response.iter_content(chunk_size=1024*1024):
+                if chunk:  # filter out keep-alive new chunks
+                    handle.write(chunk)
+    return local_filename
+
 
 def get_feed(client, feed_name):
     for cat in client.get_categories(unread_only=True):
         for feed in cat.feeds():
-                if feed.title == feed_name:
-                        return feed
+            if feed.title == feed_name:
+                return feed
+
 
 def get_headlines(client, feed_name):
-        feeds = get_feed(client, feed_name)
-        if(type(feeds) is None):
-                return None
-        else:
-                headlines = feeds.headlines()
-                return headlines
+    feeds = get_feed(client, feed_name)
+    if(type(feeds) is None):
+        return None
+    else:
+        headlines = feeds.headlines()
+        return headlines
+
 
 def get_unread_articles(client, feed_name):
-        headlines = get_headlines(client, feed_name)
-        
-        if headlines is None:
-                return None
-        else:
-                l = []
-                for headline in headlines:
-                        art = headline.full_article()
-                        if art.unread == True:
-                                l.append(art)
-                return l
+    headlines = get_headlines(client, feed_name)
+
+    if headlines is None:
+        return None
+    else:
+        l = []
+        for headline in headlines:
+            art = headline.full_article()
+            if art.unread == True:
+                l.append(art)
+        return l
+
 
 def article_trim(client, articles, release_type, count):
-        if count == 0:
-                return articles
-        elif release_type == 'rolling':
-                articles = sorted(articles, key=lambda x: x.updated, reverse=True)      
-                mark_article_read(client,articles[count:])
-                return articles[:count]
-        elif release_type == 'completion':
-                articles = sorted(articles, key=lambda x: x.updated)                    
-                return articles[:count]
-        
+    if count == 0:
+        return articles
+    elif release_type == 'rolling':
+        articles = sorted(articles, key=lambda x: x.updated, reverse=True)
+        mark_article_read(client, articles[count:])
+        return articles[:count]
+    elif release_type == 'completion':
+        articles = sorted(articles, key=lambda x: x.updated)
+        return articles[:count]
+
+
 def filtered_download(article, get_filter, feed_name):
-        main_page = requests.get(article.link)
-        soup = BeautifulSoup(main_page.text, 'html.parser')
-        paths = get_filter.split(':')
-        a = None
-        for item in paths:
-                if '*' in item:
-                        item = item.replace('*', '')
-                        a = [ a for a in soup.find_all(item)]
-                elif '#' in item:
-                        item = item.replace('#', '')
-                        if item not in article.title:
-                            return None
+    main_page = requests.get(article.link)
+    soup = BeautifulSoup(main_page.text, 'html.parser')
+    paths = get_filter.split(':')
+    a = None
+    for item in paths:
+        if '*' in item:
+            item = item.replace('*', '')
+            a = [a for a in soup.find_all(item)]
+        elif '#' in item:
+            item = item.replace('#', '')
+            if item not in article.title:
+                return None
 
-                elif '==' in item:
-                        l = []
-                        item = item.split('==')
-                        for k in a:         
-                                if k.get(item[0]) == item[1]:
-                                        l.append(k)
-                        a = l
+        elif '==' in item:
+            l = []
+            item = item.split('==')
+            for k in a:
+                if k.get(item[0]) == item[1]:
+                    l.append(k)
+            a = l
 
-                elif '...->' in item:
-                        l = []
-                        item = item.split('...->')
-                        for k in a:       
-                                if k.parent.parent.get(item[1]) != None and item[0] in k.parent.parent.get(item[1]):
-                                        l.append(k)
-                        a = l
-                elif '..->' in item:
-                        l = []
-                        item = item.split('..->')
-                        for k in a:       
-                                if k.parent.get(item[1]) != None and item[0] in k.parent.get(item[1]):
-                                        l.append(k)
-                        a = l
-                elif '->' in item:
-                        l = []
-                        item = item.split('->')
-                        for k in a:       
-                                if k.get(item[1]) != None and item[0] in k.get(item[1]):
-                                        l.append(k)
-                        a = l
-        
-                elif '!' in item:
-                        item = item.replace('!', '')
-                        if len(a) > 0:
-                                a = a[0]
-                                return download_item(a.get(item), feed_name)
-                        else:
-                                return None
-                elif '/' in item:
-                        item = item.replace('/','')
-                        if len(a) > 0:
-                                a = a[0]
-                                return download_item('http://'+article.link.split('/')[2]+a.get(item),feed_name)
-                        else:
-                                return None
-                
-                if item == 'attachment':
-                        return download_item(article.attachments[0]['1'],feed_name)
+        elif '...->' in item:
+            l = []
+            item = item.split('...->')
+            for k in a:
+                if k.parent.parent.get(item[1]) != None and item[0] in k.parent.parent.get(item[1]):
+                    l.append(k)
+            a = l
+        elif '..->' in item:
+            l = []
+            item = item.split('..->')
+            for k in a:
+                if k.parent.get(item[1]) != None and item[0] in k.parent.get(item[1]):
+                    l.append(k)
+            a = l
+        elif '->' in item:
+            l = []
+            item = item.split('->')
+            for k in a:
+                if k.get(item[1]) != None and item[0] in k.get(item[1]):
+                    l.append(k)
+            a = l
+
+        elif '!' in item:
+            item = item.replace('!', '')
+            if len(a) > 0:
+                a = a[0]
+                return download_item(a.get(item), feed_name)
+            else:
+                return None
+        elif '/' in item:
+            item = item.replace('/', '')
+            if len(a) > 0:
+                a = a[0]
+                return download_item('http://'+article.link.split('/')[2]+a.get(item), feed_name)
+            else:
+                return None
+
+        if item == 'attachment':
+            return download_item(article.attachments[0]['1'], feed_name)
+
 
 def sync_items(client, db):
-        listdir = os.listdir()                                  # grabs local items
-        db_items = db.getItems()                                # grabs database items
-        for item in db_items:                                   
-                if item[1] not in listdir:                      # if the name of the database item is not local
-                        db.removeItem(item[0])                  # remove it from the database
-                        mark_article_read(client,item[0])       # mark it as read on ttrss (if the item was deleted)
-        db_locs = [name[1] for name in db_items] 
-        for item in listdir:
-                if item not in db_locs:
-                        os.remove(item)                         # if the item is not in the database remove it
+    listdir = os.listdir()                                  # grabs local items
+    db_items = db.getItems()                                # grabs database items
+    for item in db_items:
+        # if the name of the database item is not local
+        if item[1] not in listdir:
+            # remove it from the database
+            db.removeItem(item[0])
+            # mark it as read on ttrss (if the item was deleted)
+            mark_article_read(client, item[0])
+    db_locs = [name[1] for name in db_items]
+    for item in listdir:
+        if item not in db_locs:
+            # if the item is not in the database remove it
+            os.remove(item)
+
 
 def trim_db(feed, db, count, release_type):
-        if release_type == 'rolling' and count != 0:
-                items = db.getItemByFeed(feed)[::-1]
-                for item in items[count:]:
-                        db.removeItem(item[0])
+    if release_type == 'rolling' and count != 0:
+        items = db.getItemByFeed(feed)[::-1]
+        for item in items[count:]:
+            db.removeItem(item[0])
 
-def download_articles(db, art, article_dict ):
-        if not db.checkItemExists(art.id):
-                        article_content = filtered_download(art,article_dict['filter'],article_dict['feed_name'])
-                        if article_content == None:
-                             mark_article_read(client, art.id)
-                        else:
-                            db.insertItem(art.id,article_content , article_dict['feed_name'], art.updated)
-        trim_db(article_dict['feed_name'],db, int(article_dict['count']),article_dict['release_type'])
+
+def download_articles(db, art, article_dict):
+    if not db.checkItemExists(art.id):
+        article_content = filtered_download(
+            art, article_dict['filter'], article_dict['feed_name'])
+        if article_content == None:
+            mark_article_read(client, art.id)
+        else:
+            db.insertItem(art.id, article_content,
+                          article_dict['feed_name'], art.updated)
+    trim_db(article_dict['feed_name'], db, int(
+        article_dict['count']), article_dict['release_type'])
+
 
 feeds = get_feeds_from_config(config)
 os.chdir(config['Main']['Data'])
 for article_dict in feeds:
-        print(article_dict)
-        articles = get_unread_articles(client, article_dict['feed_name'])
-        if articles is None:
-                articles = []
-        article_ids =  [article.id for article in articles]
-        for db_article in db.getItemByFeed(article_dict['feed_name']):                
-                if db_article[0] not in article_ids:
-                        db.removeItem(db_article[0])
-        articles = article_trim(client,articles,article_dict['release_type'],int(article_dict['count']))
+    print(article_dict)
+    articles = get_unread_articles(client, article_dict['feed_name'])
+    if articles is None:
+        articles = []
+    article_ids = [article.id for article in articles]
+    for db_article in db.getItemByFeed(article_dict['feed_name']):
+        if db_article[0] not in article_ids:
+            db.removeItem(db_article[0])
+    articles = article_trim(
+        client, articles, article_dict['release_type'], int(article_dict['count']))
 
-        input_articles = [(rss_db(config), art, article_dict) for art in articles]                 # has to create  new database object because of threads
-        p = ThreadPool(4)
-        p.starmap(download_articles,input_articles)                
+    # has to create  new database object because of threads
+    input_articles = [(rss_db(config), art, article_dict) for art in articles]
+    p = ThreadPool(4)
+    p.starmap(download_articles, input_articles)
 sync_items(client, db)
